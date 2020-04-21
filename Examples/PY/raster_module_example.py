f899d0601a358ffaffbd8574d1dd43c6e04f473b
Shervin Azadi <shervinazadi.arch@gmail.com>
2020-04-21 14:56:37 +0200

    workshop files

--- Examples/PY/raster_module_example.py
+++ Examples/PY/raster_module_example.py
@@ -24,8 +24,7 @@ __status__ = "Dev"
 # INPUTS
 ####################################################
 
-
-vs = 0.04
+vs = 0.01
 voxel_size = np.array([vs, vs, vs])
 tol = 1e-06
 geo_mesh = Mesh.from_obj('Examples/IN/bunny.obj')
@@ -34,9 +33,8 @@ geo_mesh = Mesh.from_obj('Examples/IN/bunny.obj')
 # Rasterization
 ####################################################
 
-
 volume, points = volpy.rasterization(
-    geo_mesh, voxel_size, multi_core_process=False, return_points=True)
+    geo_mesh, voxel_size, multi_core_process=True, return_points=True)
 
 ####################################################
 # OUTPUTS
@@ -65,3 +63,35 @@ volpy.pnts_to_csv(points, pnt_filepath, metadata=pnt_metadata)
 ####################################################
 # Visualization : PyVista
 ####################################################
+
+values = volume
+
+# Create the spatial reference
+grid = pv.UniformGrid()
+
+# Set the grid dimensions: shape + 1 because we want to inject our values on
+#   the CELL data
+grid.dimensions = np.array(values.shape) + 1
+
+# retrieve the bounding box information
+mesh_bb = np.array(geo_mesh.bounding_box())
+mesh_bb_min = np.amin(mesh_bb, axis=0)
+
+# Edit the spatial reference
+grid.origin = mesh_bb_min  # The bottom left corner of the data set
+grid.spacing = voxel_size  # These are the cell sizes along each axis
+
+# Add the data values to the cell data
+grid.cell_arrays["values"] = values.flatten(order="F")  # Flatten the array!
+
+# filtering
+threshed = grid.threshold([0.5, 1.5])
+outline = grid.outline()
+
+# Now plot the grid!
+# grid.plot(show_edges=True)
+
+p = pv.Plotter()
+p.add_mesh(outline, color="k")
+p.add_mesh(threshed, show_edges=True, color="white")
+p.show()
--- Examples/PY/rasterisation_vis.py
+++ Examples/PY/rasterisation_vis.py
@@ -8,6 +8,9 @@ import numpy as np
 import pandas as pd
 import compas
 from compas.datastructures import Mesh
+import sys
+import pyvista as pv
+# np.set_printoptions(threshold=sys.maxsize)
 
 __author__ = "Shervin Azadi, and Pirouz Nourian"
 __copyright__ = "???"
@@ -69,40 +72,84 @@ ray_dir = np.concatenate(tuple(ray_dir), axis=0)
 
 hit_positions = []
 # iterate over the faces
-for face in geo_mesh.faces():
-    face_verticies_xyz = geo_mesh.face_coordinates(face)
-    if len(face_verticies_xyz) != 3:
-        continue
+# for face in geo_mesh.faces():
+faces = list(geo_mesh.faces())
+face = faces[1]
+print(face)
+face_verticies_xyz = geo_mesh.face_coordinates(face)
+"""if len(face_verticies_xyz) != 3:
+    continue"""
 
-    face_verticies_xyz = np.array(face_verticies_xyz)
+face_verticies_xyz = np.array(face_verticies_xyz)
 
-    # project the ray origin
-    proj_ray_orig = ray_orig_ind * voxel_size * (1 - ray_dir)
+# project the ray origin
+proj_ray_orig = ray_orig_ind * voxel_size * (1 - ray_dir)
 
-    # check if any coordinate of the projected ray origin is in betwen the max and min of the coordinates of the face
-    min_con = np.amin(face_verticies_xyz, axis=0) <= proj_ray_orig
-    max_con = np.amax(face_verticies_xyz, axis=0) >= proj_ray_orig
-    in_range_rays = np.any(min_con * max_con, axis=1)
+# check if any coordinate of the projected ray origin is in betwen the max and min of the coordinates of the face
+min_con = np.amin(face_verticies_xyz, axis=0) <= proj_ray_orig
+max_con = np.amax(face_verticies_xyz, axis=0) >= proj_ray_orig
+in_range_rays = np.any(min_con * max_con, axis=1)
 
-    # retrieve the ray indicies that are in range
-    in_rang_ind = np.argwhere(in_range_rays).flatten()
+# retrieve the ray indicies that are in range
+in_rang_ind = np.argwhere(in_range_rays).flatten()
+#print(np.amin(face_verticies_xyz, axis=0))
+#print(np.amax(face_verticies_xyz, axis=0))
+#print(np.c_[proj_ray_orig, min_con, max_con, in_range_rays])
+sel = ray_orig_ind[in_rang_ind] - mesh_bb_min_z3
 
-    # iterate over the rays
-    for ray in in_rang_ind:
+faces = np.array([3, 0, 1, 2])
+surf = pv.PolyData(face_verticies_xyz, faces)
 
-        # calc ray origin position: Z3 to R3
-        orig_pos = ray_orig_ind[ray] * voxel_size
-        # retrieve ray direction
-        direction = ray_dir[ray]
-        # calc the destination of ray (max distance that it needs to travel)
-        # this line has a problem given the negative indicies are included now
-        dest_pos = orig_pos + ray_dir[ray] * mesh_bb_size
+values = np.zeros(vol_size)
+sel = np.transpose(sel)
+#np.put(values, sel, 1)
+values[sel[0], sel[1], sel[2]] = 1
+# Create the spatial reference
+grid = pv.UniformGrid()
 
-        # intersction
-        hit_pt = compas.geometry.intersection_line_triangle(
-            (orig_pos, dest_pos), face_verticies_xyz, tol=tol)
-        if hit_pt is not None:
-            hit_positions.append(hit_pt)
+# Set the grid dimensions: shape + 1 because we want to inject our values on
+#   the CELL data
+grid.dimensions = np.array(values.shape) + 1
+
+# retrieve the bounding box information
+mesh_bb = np.array(geo_mesh.bounding_box())
+mesh_bb_min = np.amin(mesh_bb, axis=0)
+
+# Edit the spatial reference
+# The bottom left corner of the data set
+grid.origin = mesh_bb_min_z3 * voxel_size - voxel_size*0.5
+grid.spacing = voxel_size  # These are the cell sizes along each axis
+
+# Add the data values to the cell data
+grid.cell_arrays["values"] = values.flatten(
+    order="F")  # Flatten the array!
+
+# filtering
+threshed = grid.threshold([0.9, 1.1])
+outline = grid.outline()
+# plot each face with a different color
+p = pv.Plotter()
+p.add_mesh(surf, color="red")
+p.add_mesh(outline, color="k")
+p.add_mesh(threshed, show_edges=True, color="white", opacity=0.7)
+p.show()
+"""
+# iterate over the rays
+for ray in in_rang_ind:
+
+    # calc ray origin position: Z3 to R3
+    orig_pos = ray_orig_ind[ray] * voxel_size
+    # retrieve ray direction
+    direction = ray_dir[ray]
+    # calc the destination of ray (max distance that it needs to travel)
+    # this line has a problem given the negative indicies are included now
+    dest_pos = orig_pos + ray_dir[ray] * mesh_bb_size
+
+    # intersction
+    hit_pt = compas.geometry.intersection_line_triangle(
+        (orig_pos, dest_pos), face_verticies_xyz, tol=tol)
+    if hit_pt is not None:
+        hit_positions.append(hit_pt)
 
 ####################################################
 # convert hit positions into volumetric data
@@ -157,3 +204,4 @@ vol_df = pd.DataFrame(
 # save to csv
 hit_unq_pos_df.to_csv('PY_OUT/bunny_voxels.csv', index=True, float_format='%g')
 vol_df.to_csv('PY_OUT/bunny_volume.csv', index=False, float_format='%g')
+"""
