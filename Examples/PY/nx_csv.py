51c33f63350f0fd208d76853a127f617627e84f6
Shervin Azadi <shervinazadi.arch@gmail.com>
2020-04-24 20:52:47 +0200

    Houdini Graph Visualizer

--- Examples/PY/nx_csv.py
+++ Examples/PY/nx_csv.py
@@ -6,7 +6,7 @@ An example of exporting networkx graph model to csv fils
 import networkx as nx
 import numpy as np
 import pandas as pd
-
+import math as m
 
 __author__ = "Shervin Azadi"
 __copyright__ = "???"
@@ -31,8 +31,10 @@ node_bb = nx.betweenness_centrality(G)
 nx.set_node_attributes(G, node_bb, 'betweenness')
 names = {i: "node "+str(i) for i in range(size)}
 nx.set_node_attributes(G, names, 'name')
+pos = {i: (m.sin(3.14*2*i/size), m.cos(3.14*2*i/size), 0) for i in range(size)}
+nx.set_node_attributes(G, pos, 'position')
 
-# add attributes to the nodes
+# add attributes to the edges
 edge_names = {(i, (i + 1) % size): "edge "+str(i) for i in range(size)}
 nx.set_edge_attributes(G, edge_names, 'name')
 weights = {(i, (i + 1) % size): 0.1*i for i in range(size)}
@@ -51,12 +53,17 @@ graph_node_names = np.array(
     list(nx.get_node_attributes(G, 'name').items()))[:, 1]
 graph_node_bbs = np.array(
     list(nx.get_node_attributes(G, 'betweenness').items()))[:, 1]
+graph_node_pos = np.array([np.array(item[1]) for item in list(
+    nx.get_node_attributes(G, 'position').items())])
 
 # merge
 graph_nodes_df = pd.DataFrame(
     {'node': graph_nodes,
      'name': graph_node_names,
      'betweenness': graph_node_bbs,
+     'P.X': graph_node_pos[:, 0],
+     'P.Y': graph_node_pos[:, 1],
+     'P.Z': graph_node_pos[:, 2],
      })
 
 ####################################################
@@ -94,9 +101,9 @@ graph_adj_matrix_df = pd.DataFrame(data=graph_adj_matrix)
 ####################################################
 # save to CSV
 ####################################################
-graph_nodes_df.to_csv('PY_OUT/graph_nodes.csv',
+graph_nodes_df.to_csv('Examples/PY_OUT/graph_nodes.csv',
                       index=True, float_format='%.3f')
-graph_edges_df.to_csv('PY_OUT/graph_edges.csv',
+graph_edges_df.to_csv('Examples/PY_OUT/graph_edges.csv',
                       index=True, float_format='%.3f')
 graph_adj_matrix_df.to_csv(
-    'PY_OUT/graph_adj_matrix.csv', index=True, float_format='%.3f')
+    'Examples/PY_OUT/graph_adj_matrix.csv', index=True, float_format='%.3f')
