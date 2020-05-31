561cb35dc7811a9fe7da697b0ce7e648f71fca7f
Shervin Azadi <shervinazadi.arch@gmail.com>
2020-05-31 18:19:00 +0200

    divergence init

--- src/volpy/diff.py
+++ src/volpy/diff.py
@@ -15,14 +15,31 @@ __email__ = "shervinazadi93@gmail.com"
 __status__ = "Dev"
 
 
-def gradient(val):
+def gradient(sfield):
 
-    # gradient component
-    dx = (val[:-2, 1:-1, 1:-1] - val[2:, 1:-1, 1:-1]) * 0.5
-    dy = (val[1:-1, :-2, 1:-1] - val[1:-1, 2:, 1:-1]) * 0.5
-    dz = (val[1:-1, 1:-1, :-2] - val[1:-1, 1:-1, 2:]) * 0.5
+    # partial derivatives
+    dx = (sfield[:-2, 1:-1, 1:-1] - sfield[2:, 1:-1, 1:-1]) * 0.5
+    dy = (sfield[1:-1, :-2, 1:-1] - sfield[1:-1, 2:, 1:-1]) * 0.5
+    dz = (sfield[1:-1, 1:-1, :-2] - sfield[1:-1, 1:-1, 2:]) * 0.5
 
     # stack gradient
-    d = np.stack([dx, dy, dz], axis=0)
+    g = np.stack([dx, dy, dz], axis=0)
+
+    return(g)
+
+
+def divergence(vfield):
+
+    X = vfield[0]
+    Y = vfield[0]
+    Z = vfield[0]
+
+    # partial derivatives
+    dx = (X[:-2, 1:-1, 1:-1] - X[2:, 1:-1, 1:-1]) * 0.5
+    dy = (Y[1:-1, :-2, 1:-1] - Y[1:-1, 2:, 1:-1]) * 0.5
+    dz = (Z[1:-1, 1:-1, :-2] - Z[1:-1, 1:-1, 2:]) * 0.5
+
+    # sum divergence
+    d = dx + dy + dz
 
     return(d)
