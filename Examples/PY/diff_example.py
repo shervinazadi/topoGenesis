bdc9d0f22fba5be66e27a147ec7b978eb1418050
Shervin Azadi <shervinazadi.arch@gmail.com>
2020-05-31 18:20:09 +0200

    divergence example

--- Examples/PY/diff_example.py
+++ Examples/PY/diff_example.py
@@ -8,7 +8,12 @@ s = 3
 X, Y, Z = np.mgrid[-s:s:30j, -s:s:30j, -s:s:30j]
 val = np.sin(X * Y * Z) / (X * Y * Z)
 
+# gradient
 grad_val = vp.gradient(val)
 
-print(grad_val.shape)
+# divergence of gradient
+div_grad_val = vp.divergence(grad_val)
+
 print(val.shape)
+print(grad_val.shape)
+print(div_grad_val.shape)
