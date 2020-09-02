0302e97be2a7c794e6babd3f1e32052b1ea81bd1
Shervin Azadi <shervinazadi.arch@gmail.com>
2020-09-02 16:17:20 +0200

    datastructure tests added

---
+++ src/topogenesis/tests/test_datastructures.py
@@ -0,0 +1,42 @@
+import topogenesis as tg
+import numpy as np
+import os
+
+file_directory = os.path.dirname(os.path.abspath(__file__))
+sample_data_path = os.path.join(os.path.dirname(file_directory), "resources", "sample_data")
+
+def test_cloud_voxelation():
+    """
+    Testing voxelation method of cloud object by sample data
+    """
+
+    # Setup
+    cloud = tg.cloud_from_csv(os.path.join(sample_data_path, "rdam_cloud.csv"))
+
+    expected_lattice = tg.lattice_from_csv(os.path.join(sample_data_path, "rdam_lattice.csv"))
+
+    # Exercise
+    computed_lattice = cloud.voxelate(1, closed=True)
+
+    # Verify
+    np.testing.assert_allclose(expected_lattice, computed_lattice, rtol=1e-6, atol=0)
+
+    # Cleanup
+
+def test_lattice_centroid():
+    """
+    Testing centroid method of lattice object by sample data
+    """
+
+    # Setup
+    lattice = tg.lattice_from_csv(os.path.join(sample_data_path, "rdam_lattice.csv"))
+
+    expected_centroids = tg.cloud_from_csv(os.path.join(sample_data_path, "rdam_centroids.csv"))
+
+    # Exercise
+    computed_centroids = lattice.centroids
+
+    # Verify
+    np.testing.assert_allclose(expected_centroids, computed_centroids, rtol=1e-6, atol=0)
+
+    # Cleanup
